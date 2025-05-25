5ae47d9c6c1d300c02d28043fa2f673a112eb942
Utsho Roy <128884452+utsho261@users.noreply.github.com>
2025-05-25 19:08:00 +0600

    Add files via upload

---
+++ lab07/task4.java
@@ -0,0 +1,23 @@
+package lab07;
+/*
+Ask the user to enter a number as a string. Convert the string to
+an integer using Integer.parseInt(). Handle
+NumberFormatException if the input is not a valid number.
+ */
+import java.util.Scanner;
+
+public class task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String s = sc.nextLine();
+
+        try {
+            int result = Integer.parseInt(s);
+            System.out.println("Result: "+result);
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
+
+    }
+}
